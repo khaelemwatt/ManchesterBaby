267d998feb288f959538b2904a7790310f3dca36
Khaelem Watt <55397210+khaelemwatt@users.noreply.github.com>
2020-11-30 19:22:57 +0000

    Added header file with memory and address sizes

---
+++ header.h
@@ -0,0 +1,8 @@
+#ifndef __MANCHESTER_BABY_HEADER__ // this is a 'guard' on the header file, to prevent it's contents from
+#define __MANCHESTER_BABY_HEADER__ // being redefined more than once during the build process
+
+// ### DATA STRUCTURES ### 
+#define MEMSIZE 32
+#define ADDRESSSIZE 32
+
+#endif
