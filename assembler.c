8921a6e8c06c34a80a35f821820ee77b6c571d77
robbieG123 <55398030+robbieG123@users.noreply.github.com>
2020-11-30 14:42:05 +0000

    read from assembler text file
    assembler can now read text from external assembly language file

--- assembler.c
+++ assembler.c
@@ -1,1 +1,25 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main()
+{
+   char ch;
+   FILE *fp;
+   char line[32];
+   //printf("Enter the name of file:: ");
+   //gets(file_name);
+   fp = fopen("BabyTestAssembler.txt","r");
+   if( fp == NULL ){
+        printf("\nCan not open the file.");
+        exit(0);
+   }
+   printf("\nThe contents of %s file is :\n", "BabyTestAssembler.txt");
+   while(fgets(line, 32, fp) != NULL){
+       if ()
+       printf("%s", line);
+   }
+      
+   fclose(fp);
+ 
+   return 0;
+}
