a4f23f744ff5fa06665d09f073ab0486f928d7f3
Khaelem Watt <55397210+khaelemwatt@users.noreply.github.com>
2020-11-30 19:22:07 +0000

    Added template functions and arrays

--- simulator.c
+++ simulator.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "header.h"
 
+//Initial global declarations
+int accumulator[MEMSIZE];
+int controlInstruction[MEMSIZE];
+int presentInstruction[MEMSIZE];
+int store[MEMSIZE][ADDRESSSIZE];
+int addressBus[MEMSIZE];
+int controlBus[MEMSIZE]
+
+void incrementCI(){
+	//Increment the control instruction by 1
+}
+
+void fetch(){
+	//Get the instruction from the data store
+}
+
+void decode(){
+	//Get the memory address of the operand and OP code from the current instruction
+}
+
+void execute(){
+	//Carry out the instruction
+}
+
+void display(){
+	//Display the contents of the data store
+}
+
+int main(){ //Main loop for the fetch execute cycle
+
+	incrementCI();
+	fetch();
+	decode();
+	execute();
+	display();
+}
